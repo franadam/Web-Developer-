1e8aa0f8b0b2e6650d46b0ba95e5919b5dae3dd5
franadam <francoisadam@hotmail.be>
2020-04-06 11:41:09 +0200

    Config linters

---
+++ my-app/Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-stylelint');
+    grunt.loadNpmTasks('grunt-sass-lint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-postcss');
+
+    grunt.initConfig({
+        eslint: {
+            options: {
+                configFile: '.eslintrc.json',
+                fix: true
+            },
+            target: ['src/components/*.js']
+        },
+        stylelint: {
+            options: {
+                configFile: '.stylelintrc.json'
+            },
+            all: ['src/scss/**/*.scss']
+        },
+        sasslint: {
+            options: {
+                configFile: '.sasslintrc.json',
+            },
+            target: ['src/scss/**/*.scss']
+        },
+        postcss: {
+            options: {
+              map: true,
+              processors: [
+                require('pixrem')(), // add fallbacks for rem units
+                require('autoprefixer')(), // add vendor prefixes
+                //require('cssnano')() // minify the result
+              ]
+            },
+            dist: {
+              src: 'src/scss/**/*.scss'
+            }
+          },
+        watch: {
+            files: ['Gruntfile.js','src/components/*.js','src/scss/**/*.scss'],
+            tasks: ['eslint', 'stylelint','sasslint']
+        }
+    });
+
+    grunt.registerTask('default', ['eslint','stylelint','sasslint']);
+    grunt.registerTask('postprocess', ['postcss']);
+};
+
