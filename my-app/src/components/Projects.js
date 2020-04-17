a5e47051dd8bb673f288b353e2703cf4e9fd40db
franadam <francoisadam@hotmail.be>
2020-04-17 14:11:43 +0200

    Add overlay on images

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -21,6 +21,7 @@ function filterSelection (c) {
 
   for (i = 0; i < x.length; i++) {
     x[i].style.display = 'none'
+    // x[i].style.transition = '.3s';
 
     if (c === 'all') {
       x[i].style.display = 'block'
@@ -45,7 +46,7 @@ function showFiltered (element, name) {
 function Projects () {
   return (
     <div className='projects' >
-      <h3>Projects</h3>
+      <h3>All of my Projects</h3>
       <div className='projects__filter'>
         <a className='btn btn__filter btn--active' id='all' onClick={() => filterSelection('all')}>All</a>
         <a className='btn btn__filter' id='css' onClick={() => filterSelection('css')}><FontAwesomeIcon icon={['fab', 'html5']} /> HTML /  <FontAwesomeIcon icon={['fab', 'css3']} /> CSS</a>
@@ -55,68 +56,130 @@ function Projects () {
 
       <div className='projects__gallery'>
 
+        <div className='project-title text-center react'>
+          <p>User Dashboard</p>
+          <img id="img-p9" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FUser%20Dashboard.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>User Dashboard</h3>
+              <p>React</p>
+              <a className='btn' href="https://eloquent-aryabhata-f6b832.netlify.app"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
+        </div>
+
         <div className='project-title text-center css'>
           <p>Tribute Page</p>
-          <a href="https://codepen.io/franadam/pen/PooLjwK"
-            target="_blank" rel="noopener noreferrer" >
-            <img id="img-p1" className="img img--project" src="https://www.spettacolo.eu/wp-content/uploads/2019/09/Mika-cover-nuovo-album.jpg" />
-          </a>
+          <img id="img-p1" className="img img--project" alt='project' src="https://www.spettacolo.eu/wp-content/uploads/2019/09/Mika-cover-nuovo-album.jpg" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Tribute Page</h3>
+              <p>HTML / CSS</p>
+              <a className='btn' href="https://codepen.io/franadam/full/PooLjwK"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
         <div className='project-title text-center css'>
           <p>Survey Form</p>
-          <a href="https://codepen.io/franadam/pen/BaabPJx"
-            target="_blank" rel="noopener noreferrer" >
-            <img id="img-p2" className="img img--project" src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FForm.png" />
-          </a>
+          <img id="img-p2" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FForm.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Survey Form</h3>
+              <p>HTML / CSS</p>
+              <a className='btn' href="https://codepen.io/franadam/full/BaabPJx"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
         <div className='project-title text-center css'>
           <p>Product Landing Page</p>
-          <a href="https://codepen.io/franadam/pen/WNNmqoy"
-            target="_blank" rel="noopener noreferrer" >
-            <img id="img-p3" className="img img--project" src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FProduct.png" />
-          </a>
+          <img id="img-p3" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FProduct.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Product Landing Page</h3>
+              <p>HTML / CSS</p>
+              <a className='btn' href="https://codepen.io/franadam/full/BaabPJx"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
         <div className='project-title text-center css'>
           <p>Technical Documentation Page</p>
-          <a href="https://codepen.io/franadam/pen/qBBwRKZ"
-            target="_blank" rel="noopener noreferrer">
-            <img id="img-p4" className="img img--project" src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FDocumentation.png" />
-          </a>
+          <img id="img-p4" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FDocumentation.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Technical Documentation Page</h3>
+              <p>HTML / CSS</p>
+              <a className='btn' href="https://codepen.io/franadam/full/qBBwRKZ"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
         <div className='project-title text-center react'>
           <p>Random Quote Machine</p>
-          <a href="https://codepen.io/franadam/pen/eYmMeJd"
-            target="_blank" rel="noopener noreferrer" >
-            <img id="img-p5" className="img img--project" src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FRandom%20Quote%20Machine.png" />
-          </a>
+          <img id="img-p5" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FRandom%20Quote%20Machine.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Random Quote Machine</h3>
+              <p>React</p>
+              <a className='btn' href="https://codepen.io/franadam/full/eYmMeJd"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
         <div className='project-title text-center react'>
           <p>Markdown Previewer</p>
-          <a href="https://codepen.io/franadam/pen/WNbJvQz"
-            target="_blank" rel="noopener noreferrer">
-            <img id="img-p6" className="img img--project" src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FMarkdown%20Preview.png" />
-          </a>
+          <img id="img-p6" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FMarkdown%20Preview.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Markdown Previewer</h3>
+              <p>React</p>
+              <a className='btn' href="https://codepen.io/franadam/full/WNbJvQz"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
         <div className='project-title text-center d3'>
-          <p>D3 Bar Chart</p>
-          <a href="https://codepen.io/franadam/pen/Exjgoag"
-            target="_blank" rel="noopener noreferrer">
-            <img id="img-p7" className="img img--project" src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FD3%20Bar%20Chart.png" />
-          </a>
+          <p>Bar Chart</p>
+          <img id="img-p7" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FD3%20Bar%20Chart.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Bar Chart</h3>
+              <p>D3</p>
+              <a className='btn' href="https://codepen.io/franadam/full/GRJqLEz"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
         <div className='project-title text-center d3'>
-          <p>D3 Scatterplot Graph</p>
-          <a href="https://codepen.io/franadam/pen/GRJqLEz"
-            target="_blank" rel="noopener noreferrer">
-            <img id="img-p8" className="img img--project" src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FD3%20Dot%20Graph.png" />
-          </a>
+          <p>Scatterplot Graph</p>
+          <img id="img-p8" className="img img--project" alt='project' src="https://cdn.glitch.com/6daee3c9-0db4-4cde-afd8-d99d15aef073%2FD3%20Dot%20Graph.png" />
+
+          <div className="overlay">
+            <div className="overlay__text">
+              <h3>Scatterplot Graph</h3>
+              <p>D3</p>
+              <a className='btn' href="https://codepen.io/franadam/full/Exjgoag"
+                target="_blank" rel="noopener noreferrer" >See Page</a>
+            </div>
+          </div>
         </div>
 
       </div>
